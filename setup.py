45607cabc1113914705c19195b29c719e4988e95
Sahil Tikale <sahil.tikale@gmail.com>
2018-11-30 14:22:35 -0500

    Adds setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+        name='flocx', 
+        version='0.1.0',
+        maintainer='Sahil Tikale',
+        maintainer_email='tikale@bu.edu',
+        url='https://github.com/CCI-MOC/flocx',
+        description='A marketplace that enables sharing and trading '
+        'of bare-metal servers among non-trusting stake-holders.',
+        license='Apache 2.0',
+        classifiers=[
+            'Development Status :: Beta',
+            'Intended Audience :: Developers',
+            'Intended Audience :: Science/Research',
+            'License :: OSI Approved :: Apache Software License, version 2.0',
+            'Topic :: System :: Cloud :: Installation/Setup',
+            'Environment :: Web Environment',
+            'Operating System :: POSIX :: Linux',
+            'Programming Language :: Python',
+            ],
+        keywords='cloud cloud-economics bare-metal setuptools data-center',
+)
+
+
+
+
